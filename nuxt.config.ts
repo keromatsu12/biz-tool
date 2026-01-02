fb0472e2ce6ea1c2d51d818f3d17606399222e70
keromatsu12 <kazu.dance.12@gmail.com>
2026-01-03 01:24:32 +0900

    chore: node:urlのパッケージ導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
+import { fileURLToPath } from "node:url";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   future: {
     compatibilityVersion: 4,
   },
-  extends: ['./layers/base'],
+  extends: ["./layers/base"],
+  // エイリアスの設定
+  alias: {
+    // #base/components/Footer.vue のように呼び出せるようにする
+    "#base": fileURLToPath(new URL("./layers/base/app", import.meta.url)),
+  },
   devtools: { enabled: true },
   vite: {
     server: {
@@ -14,4 +21,4 @@ export default defineNuxtConfig({
       },
     },
   },
-})
+});
